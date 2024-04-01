400b8b281805e45b044e5f22e64b30c9d95989d7
appreal243 <apprgame243@gmail.com>
2024-04-02 02:44:47 +0700

    update portfolio : menambahkan animasi

--- js/script.js
+++ js/script.js
@@ -53,3 +53,29 @@ menuIcon.onclick = () => {
   menuIcon.classList.toggle("bx-x");
   navbar.classList.toggle("active");
 };
+
+
+// Scroll reveal
+ScrollReveal({
+  // reset: true,
+  distance: "80px",
+  duration: 2000,
+  delay: 200,
+});
+
+ScrollReveal().reveal(".home-content, .heading", { origin: "top" });
+ScrollReveal().reveal(
+  ".home-img, .services-container, .portfolio-box, .contact form",
+  { origin: "bottom" }
+);
+ScrollReveal().reveal(".home-content h1, .about-img", { origin: "left" });
+ScrollReveal().reveal(".home-content p, .about-content", { origin: "right" });
+
+// Typed Js
+const typed = new Typed(".multiple-text", {
+  strings: ["Frontend Developer", "UI/UX Design", "Backend Developer"],
+  typeSpeed: 100, 
+  backSpeed: 100,
+  backDelay: 1000,
+  loop: true
+});
